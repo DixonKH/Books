92b35a086a5198744181c6e458ea0d2160203cfb
DixonKH <khaydarovdilshod9@gmail.com>
2025-03-02 01:17:52 +0900

    feat: connected mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import mongoose from 'mongoose';
 
-/** 1-ENTRANCE */
-
-/** 2-ROUTING */
-
-/** 3- */
+mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
+    console.log('Connected to the database');
+    const PORT = process.env.PORT ?? 3000;
+})
+.catch((error) => {
+    console.log('Error connecting to the database');
+    console.log(error);
+});
