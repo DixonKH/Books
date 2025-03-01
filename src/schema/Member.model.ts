786d3b9f2bc14291b97bd7794b3c9e8119d9a401
DixonKH <khaydarovdilshod9@gmail.com>
2025-03-02 06:52:22 +0900

    feat: develop MemberSchema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+
+const memberSchema = new Schema({
+    memberNick: {
+        type: String,
+        index: { unique: true, sparse: true },
+        required: true
+    },
+    memberPassword: {
+        type: String,
+        required: true
+    },
+    memberImage: {
+        type: String
+    }
+},
+{
+    timestamps: true
+});
+
+export default mongoose.model('Member', memberSchema);
