e5b190aa9cd35cb4d89267e680f273c4154d8365
DixonKH <khaydarovdilshod9@gmail.com>
2025-03-02 01:54:51 +0900

    feat: intigration of expressjs

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { urlencoded } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // Get the file path of the current module
+const __dirname = path.dirname(__filename); // Get the directory path of the current module
+
+/** 1-ENTRANCE */
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+
+/** 2-SESSIONS */
+
+/** 3-VIEWS */
+
+/** 4-ROUTES */
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
+import app from './app';
 
 mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
     console.log('Connected to the database');
     const PORT = process.env.PORT ?? 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    })
 })
 .catch((error) => {
     console.log('Error connecting to the database');
